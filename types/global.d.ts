8427884be868f377d322bafa5c36542c51234386
yuanlee <yuanlee@163.com>
2023-08-08 21:53:23 +0800

    fix: Typesscript 全局类型定义

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,2 @@
-declare global {
-  declare type Recordable<T = any> = Record<string, T>;
-  declare type Nullable<T> = T | null;
-}
-
+declare type Recordable<T = any> = Record<string, T>;
+declare type Nullable<T> = T | null;
